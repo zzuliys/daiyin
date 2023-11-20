f707630de02e7ec35494c0083c27a2f41e9c67b3
zzuliys <68427285+zzuliys@users.noreply.github.com>
2023-11-20 10:21:34 +0800

    Delete DesignPatterns/build directory

--- DesignPatterns/build/CMakeFiles/3.27.0-rc5/CMakeRCCompiler.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_RC_COMPILER "C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/VC/Tools/MSVC/14.29.30133/bin/Hostx64/x64/rc.exe")
-set(CMAKE_RC_COMPILER_ARG1 "")
-set(CMAKE_RC_COMPILER_LOADED 1)
-set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
-set(CMAKE_RC_OUTPUT_EXTENSION .res)
-set(CMAKE_RC_COMPILER_ENV_VAR "RC")
